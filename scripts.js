2f66754c62158920d91ab0b57e163ab97c12263b
Michael <michaeleburrell@gmail.com>
2020-07-30 17:43:54 -0700

    removed extra console logs

--- scripts.js
+++ scripts.js
@@ -81,7 +81,6 @@ function getVideos() {
 }
 
 function addVideos(videos) {
-  console.log(videos);
   videos.forEach((video) => {
     let li = document.createElement("li");
     li.innerHTML = video.created_at;
@@ -191,18 +190,15 @@ function checkForControls(transcript) {
 }
 
 recognition.addEventListener("result", (e) => {
-  console.log(e);
   const transcript = Array.from(e.results)
     .map((result) => result[0])
     .map((result) => result.transcript)
     .join("");
 
   if (e.results[0].isFinal) {
-    console.log(transcript);
     checkForControls(transcript);
   }
 });
 
 recognition.addEventListener("end", recognition.start);
-console.log(recognition);
 recognition.start();
